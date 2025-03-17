1b0d0552fc06da96bb651961444f59b136b3f984
magdalenapetrushevska <magdalenapetrusevska9@gmail.com>
2025-03-17 22:08:09 +0100

    add tests for share locaton layout page

--- cypress/e2e/location.cy.js
+++ cypress/e2e/location.cy.js
@@ -1,8 +1,23 @@
 /// <reference types="cypress" />
 
-describe('share location', () => {
-  it('should fetch the user location', () => {
+describe('Share Location', () => {
+  beforeEach(() => {
     cy.visit('/');
-    cy.get('[data-cy="get-loc-btn"]').click();
+  });
+
+  it('should display the correct page title', () => {
+    cy.get('h1').should('have.length', 1).and('have.text', 'Share Location');
+  });
+
+  it('should have the share location button disabled', () => {
+    cy.get('[data-cy="share-loc-btn"]').should('have.attr', 'disabled');
+  });
+
+  it('should focus and blur the name input', () => {
+    cy.get('[data-cy="name-input"]').focus().blur();
+  });
+
+  it('should have the get location button enabled', () => {   
+    cy.get('[data-cy="get-loc-btn"]').should('not.have.attr', 'disabled');
   });
 });
