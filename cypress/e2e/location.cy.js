d66482482fac4e8ab1ccfb3f5bd2fdb78cb6f6db
magdalenapetrushevska <magdalenapetrusevska9@gmail.com>
2025-03-18 21:45:42 +0100

    add test for permission denined

--- cypress/e2e/location.cy.js
+++ cypress/e2e/location.cy.js
@@ -1,40 +1,41 @@
 /// <reference types="cypress" />
 
 describe("Share Location", () => {
-  it("should display the correct page title", () => {
+  beforeEach(() => {
     cy.visit("/");
+  });
+
+  it("should display the correct page title", () => {
     cy.get("h1").should("have.length", 1).and("have.text", "Share Location");
   });
 
   it("should have the share location button disabled", () => {
-    cy.visit("/");
     cy.get('[data-cy="share-loc-btn"]').should("have.attr", "disabled");
   });
 
   it("should focus and blur the name input", () => {
-    cy.visit("/");
     cy.get('[data-cy="name-input"]').focus().blur();
   });
 
   it("should have the get location button enabled", () => {
-    cy.visit("/");
     cy.get('[data-cy="get-loc-btn"]').should("not.have.attr", "disabled");
   });
 
   it("should fetch the user location", () => {
-    cy.visit("/").then((win) =>
+    cy.window().then((win) =>
       cy
         .spy(win.navigator.geolocation, "getCurrentPosition")
         .as("getCurrentPosition")
     );
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get("@getCurrentPosition").should("have.been.called");
-    cy.get('[data-cy="share-loc-btn"]').should("be.disabled");
-    cy.get('[data-cy="actions"]').contains("Location fetched!");
+    cy.get('[data-cy="actions"]')
+      .should("be.visible")
+      .and("contain", "Location fetched!");
   });
 
   it("should receive error message when input field is empty", () => {
-    cy.visit("/").then((win) =>
+    cy.window().then((win) =>
       cy
         .spy(win.navigator.geolocation, "getCurrentPosition")
         .as("getCurrentPosition")
@@ -42,9 +43,27 @@ describe("Share Location", () => {
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get("@getCurrentPosition").should("have.been.called");
     cy.get('[data-cy="share-loc-btn"]').click();
-    cy.get('[data-cy="error-message"]').contains("Please enter your name");
+    cy.get('[data-cy="error-message"]')
+      .should("be.visible")
+      .and("contain", "Please enter your name");
   });
 
+  it("should display an error message when geolocation is not available or permission is denied", () => {
+    cy.window().then((win) =>
+      cy
+        .stub(win.navigator.geolocation, "getCurrentPosition")
+        .callsFake((success, error) => {
+          error();
+        })
+    );
 
+    cy.get('[data-cy="get-loc-btn"]').click();
 
+    cy.get('[data-cy="info-message"]')
+      .should("be.visible")
+      .and(
+        "contain",
+        "Your browser or permission settings do not allow location fetching"
+      );
+  });
 });
--- main.js
+++ main.js
@@ -2,36 +2,39 @@ const user = {
   location: {
     lat: 0,
     lng: 0,
-    url: '',
+    url: "",
   },
 };
 
 function getUserLocation(event) {
   const clickedBtn = event.target;
   const container = clickedBtn.parentNode;
-  if ('geolocation' in navigator) {
+  if ("geolocation" in navigator) {
     clickedBtn.disabled = true;
     clickedBtn.innerHTML = '<span class="loader"></span>';
-    navigator.geolocation.getCurrentPosition(function (position) {
-      user.location.lat = position.coords.latitude;
-      user.location.lng = position.coords.longitude;
-      user.location.url = `https://www.bing.com/maps?cp=${user.location.lat}~${user.location.lng}&lvl=15&style=r`;
-      container.insertBefore(
-        document.createTextNode('Location fetched!'),
-        clickedBtn
-      );
-      container.querySelector('svg').classList.add('active');
-      container.removeChild(clickedBtn);
-      container.querySelector('button').disabled = false;
-      container.querySelector('button').classList.add('active');
-    }, () => {
-      displayInfoMessage(
-        'Your browser or permission settings do not allow location fetching.'
-      );
-    });
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        user.location.lat = position.coords.latitude;
+        user.location.lng = position.coords.longitude;
+        user.location.url = `https://www.bing.com/maps?cp=${user.location.lat}~${user.location.lng}&lvl=15&style=r`;
+        container.insertBefore(
+          document.createTextNode("Location fetched!"),
+          clickedBtn
+        );
+        container.querySelector("svg").classList.add("active");
+        container.removeChild(clickedBtn);
+        container.querySelector("button").disabled = false;
+        container.querySelector("button").classList.add("active");
+      },
+      () => {
+        displayInfoMessage(
+          "Your browser or permission settings do not allow location fetching."
+        );
+      }
+    );
   } else {
     displayInfoMessage(
-      'Your browser or permission settings do not allow location fetching.'
+      "Your browser or permission settings do not allow location fetching."
     );
   }
 }
@@ -40,23 +43,23 @@ function shareLocation(event) {
   // Use clipboard API to copy the location to the clipboard
   event.preventDefault();
   const fd = new FormData(event.target);
-  const userName = fd.get('name');
+  const userName = fd.get("name");
 
   if (
-    userName.trim() === '' ||
+    userName.trim() === "" ||
     user.location.lat === 0 ||
     user.location.lng === 0
   ) {
-    document.getElementById('error').textContent =
-      'Please enter your name and get your location first!';
+    document.getElementById("error").textContent =
+      "Please enter your name and get your location first!";
     return;
   }
 
-  document.getElementById('error').textContent = '';
+  document.getElementById("error").textContent = "";
 
   const storedUrl = localStorage.getItem(userName);
   if (storedUrl) {
-    copyToClipboard(storedUrl, 'Stored location URL copied to clipboard.');
+    copyToClipboard(storedUrl, "Stored location URL copied to clipboard.");
     return;
   }
 
@@ -65,17 +68,17 @@ function shareLocation(event) {
   }_${encodeURI(userName)}`;
 
   localStorage.setItem(userName, user.location.url);
-  copyToClipboard(user.location.url, 'Location URL copied to clipboard.');
+  copyToClipboard(user.location.url, "Location URL copied to clipboard.");
 }
 
 function copyToClipboard(data, infoText) {
-  if ('clipboard' in navigator) {
+  if ("clipboard" in navigator) {
     navigator.clipboard.writeText(data).then(
       function () {
         displayInfoMessage(infoText);
       },
       function () {
-        displayInfoMessage('Failed to copy location URL to clipboard.');
+        displayInfoMessage("Failed to copy location URL to clipboard.");
       }
     );
   }
@@ -87,16 +90,16 @@ function displayInfoMessage(message) {
   if (existingTimer) {
     clearTimeout(existingTimer);
   }
-  const infoMsg = document.getElementById('info-message');
-  infoMsg.querySelector('p').textContent = message;
-  infoMsg.classList.add('visible');
+  const infoMsg = document.getElementById("info-message");
+  infoMsg.querySelector("p").textContent = message;
+  infoMsg.classList.add("visible");
   existingTimer = setTimeout(() => {
-    infoMsg.classList.remove('visible');
+    infoMsg.classList.remove("visible");
   }, 2000);
 }
 
-const getLocBtn = document.getElementById('get-location');
-const form = document.querySelector('form');
+const getLocBtn = document.getElementById("get-location");
+const form = document.querySelector("form");
 
-getLocBtn.addEventListener('click', getUserLocation);
-form.addEventListener('submit', shareLocation);
+getLocBtn.addEventListener("click", getUserLocation);
+form.addEventListener("submit", shareLocation);
